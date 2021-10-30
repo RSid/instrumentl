1f6d78b971086ce5e27f9c859c52f993447fd078
Alla <sm1th303@gmail.com>
2021-10-30 18:57:13 -0400

    stubbed out some exploration of parsing awards

--- app/controllers/filing_parser_controller.rb
+++ app/controllers/filing_parser_controller.rb
@@ -3,13 +3,20 @@ class FilingParserController < ApplicationController
         file = File.join(Rails.root, 'app', 'example1.xml')
         doc = File.open(file) { |f| Nokogiri::XML(f) }
 
-        debugger
-        parsed_ein = doc.at('Return/ReturnHeader/Filer/EIN').text
-        parsed_name = doc.at('Return/ReturnHeader/Filer/Name/BusinessNameLine1').text
-        parsed_address = doc.at('Return/ReturnHeader/Filer/USAddress/AddressLine1').text
-        parsed_city = doc.at('Return/ReturnHeader/Filer/USAddress/City').text
-        parsed_state = doc.at('Return/ReturnHeader/Filer/USAddress/State').text
-        parsed_zip = doc.at('Return/ReturnHeader/Filer/USAddress/ZIPCode').text
+        filer = parse_filer(doc)
+        award = parse_award(doc)
+        render json: {filer: filer, award: award}
+    end
+
+    private
+
+    def parse_filer(xml_doc)
+        parsed_ein = xml_doc.at('Return/ReturnHeader/Filer/EIN').text
+        parsed_name = xml_doc.at('Return/ReturnHeader/Filer/Name/BusinessNameLine1').text
+        parsed_address = xml_doc.at('Return/ReturnHeader/Filer/USAddress/AddressLine1').text
+        parsed_city = xml_doc.at('Return/ReturnHeader/Filer/USAddress/City').text
+        parsed_state = xml_doc.at('Return/ReturnHeader/Filer/USAddress/State').text
+        parsed_zip = xml_doc.at('Return/ReturnHeader/Filer/USAddress/ZIPCode').text
 
         filer = Filer.new(ein: parsed_ein, 
             name: parsed_name, 
@@ -17,7 +24,18 @@ class FilingParserController < ApplicationController
             city: parsed_city, 
             state: parsed_state, 
             zip: parsed_zip)
+        return filer
+    end
 
-        render json: filer
+    def parse_award(xml_doc)
+        recipients = xml_doc.at('Return/ReturnData/IRS990ScheduleI/RecipientTable')
+        #xml_doc.at('Return/ReturnData/IRS990ScheduleI').element_children[5].element_children[0].at('BusinessNameLine1').text
+        debugger
+        parsed_recipient_name = xml_doc.at('Return/ReturnData/IRS990ScheduleI/RecipientTable/RecipientNameBusiness/BusinessNameLine1').text
+
+        parsed_purpose = xml_doc.at('Return/ReturnData/IRS990ScheduleI/RecipientTable/PurposeOfGrant').text
+        parsed_cash_amount = xml_doc.at('Return/ReturnData/IRS990ScheduleI/RecipientTable/AmountOfCashGrant').text
+        award = Award.new(purpose: parsed_purpose, cash_amount: parsed_cash_amount)
+        return award
     end
 end
