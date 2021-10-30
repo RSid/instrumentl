3419f40bda689dabe7db33b9f961e49f5477bc62
Alla <sm1th303@gmail.com>
2021-10-30 18:21:03 -0400

    parses all the filer properties correctly, wouldn't fail gracefully if missing an xml element

--- app/controllers/filing_parser_controller.rb
+++ app/controllers/filing_parser_controller.rb
@@ -4,9 +4,19 @@ class FilingParserController < ApplicationController
         doc = File.open(file) { |f| Nokogiri::XML(f) }
 
         debugger
+        parsed_ein = doc.at('Return/ReturnHeader/Filer/EIN').text
         parsed_name = doc.at('Return/ReturnHeader/Filer/Name/BusinessNameLine1').text
+        parsed_address = doc.at('Return/ReturnHeader/Filer/USAddress/AddressLine1').text
+        parsed_city = doc.at('Return/ReturnHeader/Filer/USAddress/City').text
+        parsed_state = doc.at('Return/ReturnHeader/Filer/USAddress/State').text
+        parsed_zip = doc.at('Return/ReturnHeader/Filer/USAddress/ZIPCode').text
 
-        filer = Filer.new(name: parsed_name)
+        filer = Filer.new(ein: parsed_ein, 
+            name: parsed_name, 
+            address: parsed_address, 
+            city: parsed_city, 
+            state: parsed_state, 
+            zip: parsed_zip)
 
         render json: filer
     end
