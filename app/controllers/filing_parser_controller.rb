2a22b975cf31ce4f913031b5edd02b4fa2909152
Alla <sm1th303@gmail.com>
2021-10-30 17:42:28 -0400

    generated filter and award model, showing the filer text on endpoint get

--- app/controllers/filing_parser_controller.rb
+++ app/controllers/filing_parser_controller.rb
@@ -3,6 +3,10 @@ class FilingParserController < ApplicationController
         file = File.join(Rails.root, 'app', 'example1.xml')
         doc = File.open(file) { |f| Nokogiri::XML(f) }
 
-        render json: { 'hi': 'yo'}
+        filer = doc.at('Return/ReturnHeader/Filer').text
+
+        puts filer.class
+
+        render json: filer
     end
 end
