6d38a539bbc79b258227805b6c24dd2c8ddd23b3
Alla <sm1th303@gmail.com>
2021-10-30 18:15:37 -0400

    formatted xml and put 1st attribute into a property on obj

--- app/controllers/filing_parser_controller.rb
+++ app/controllers/filing_parser_controller.rb
@@ -3,9 +3,10 @@ class FilingParserController < ApplicationController
         file = File.join(Rails.root, 'app', 'example1.xml')
         doc = File.open(file) { |f| Nokogiri::XML(f) }
 
-        filer = doc.at('Return/ReturnHeader/Filer').text
+        debugger
+        parsed_name = doc.at('Return/ReturnHeader/Filer/Name/BusinessNameLine1').text
 
-        puts filer.class
+        filer = Filer.new(name: parsed_name)
 
         render json: filer
     end
