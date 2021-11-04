4594abb13f8fe69f15f8f13ea27e31a5dd7bf283
Alla <sm1th303@gmail.com>
2021-11-03 23:50:08 -0400

    cleaned up debugging statements, more SRPish

--- app/controllers/filing_parser_controller.rb
+++ app/controllers/filing_parser_controller.rb
@@ -11,8 +11,6 @@ class FilingParserController < ApplicationController
                 'app/201831359349101003_public.xml',
                 'app/201921719349301032_public.xml']
         
-        single_test = ['app/201401839349300020_public.xml']
-
         for f in file_list do 
             file = File.join(Rails.root, f)
             xml_doc = File.open(file) { |f| Nokogiri::XML(f) }
@@ -23,14 +21,23 @@ class FilingParserController < ApplicationController
     
             for recipient_award_element in award_and_recipient_list do
                 #skipping the RecordsMaintained element, and any others that don't have more data
-                parse_and_create_award(recipient_award_element, filer.id)
+                if recipient_award_element.element_children.length > 0
+                    award = parse_award(recipient_award_element, filer.id)
+                    receiver = parse_receiver(recipient_award_element)
+
+                    award.build_receiver(ein: receiver.ein, 
+                        name: receiver.name, 
+                        address: receiver.address, 
+                        city: receiver.city, 
+                        state: receiver.state, 
+                        zip: receiver.zip)       
+                    #debugger     
+                    #TODO: slow, let's figure out saving in bulk
+                    award.save
                 end
             end
-            
-            #render json: {filer: filer, awards: awards.to_json(include: :receiver)}
         end
-        debugger
-        render json: {ok: 200}
+        render json: {Created: 204}
     end
 
     private
@@ -75,67 +82,57 @@ class FilingParserController < ApplicationController
         return filer
     end
 
-    def parse_and_create_award(recipient_award_element, filer_id)
-        if recipient_award_element.element_children.length > 0
-            parsed_grant_purpose ||= recipient_award_element.element_children.at('PurposeOfGrant')&.text
-            parsed_grant_purpose ||= recipient_award_element.element_children.at('PurposeOfGrantTxt')&.text
-            parsed_grant_purpose ||= recipient_award_element.element_children.at('GrantOrContributionPurposeTxt')&.text
-            
-            parsed_cash_amount ||= recipient_award_element.element_children.at('AmountOfCashGrant')&.text
-            parsed_cash_amount ||= recipient_award_element.element_children.at('CashGrantAmt')&.text
-            parsed_cash_amount ||= recipient_award_element.element_children.at('Amt')&.text
-            
-            if parsed_grant_purpose.nil? || parsed_cash_amount.nil?
-                #depends on shape of other files whether we can skip these or need better parsing
-                puts recipient_award_element.element_children
-                #next
-            end
-            
-            award = Award.new(filer_id: filer_id, 
-                purpose: parsed_grant_purpose, 
-                cash_amount: parsed_cash_amount)
+    def parse_award(recipient_award_element, filer_id)
+        parsed_grant_purpose ||= recipient_award_element.element_children.at('PurposeOfGrant')&.text
+        parsed_grant_purpose ||= recipient_award_element.element_children.at('PurposeOfGrantTxt')&.text
+        parsed_grant_purpose ||= recipient_award_element.element_children.at('GrantOrContributionPurposeTxt')&.text
+        
+        parsed_cash_amount ||= recipient_award_element.element_children.at('AmountOfCashGrant')&.text
+        parsed_cash_amount ||= recipient_award_element.element_children.at('CashGrantAmt')&.text
+        parsed_cash_amount ||= recipient_award_element.element_children.at('Amt')&.text
+        
+        award = Award.new(filer_id: filer_id, 
+            purpose: parsed_grant_purpose, 
+            cash_amount: parsed_cash_amount)
 
-            parsed_recipient_ein ||= recipient_award_element.element_children.at('EINOfRecipient')&.text
-            parsed_recipient_ein ||= recipient_award_element.element_children.at('RecipientEIN')&.text
+        return award
+    end
 
-            parsed_recipient_name ||= recipient_award_element.element_children.at('RecipientNameBusiness/BusinessNameLine1')&.text
-            parsed_recipient_name ||= recipient_award_element.element_children.at('RecipientBusinessName/BusinessNameLine1Txt')&.text
-            parsed_recipient_name ||= recipient_award_element.element_children.at('RecipientBusinessName/BusinessNameLine1')&.text
+    def parse_receiver(recipient_award_element)
+        parsed_recipient_ein ||= recipient_award_element.element_children.at('EINOfRecipient')&.text
+        parsed_recipient_ein ||= recipient_award_element.element_children.at('RecipientEIN')&.text
 
-            parsed_recipient_address ||= recipient_award_element.element_children.at('AddressUS/AddressLine1')&.text
-            parsed_recipient_address ||= recipient_award_element.element_children.at('USAddress/AddressLine1Txt')&.text
-            parsed_recipient_address ||= recipient_award_element.element_children.at('USAddress/AddressLine1')&.text
-            parsed_recipient_address ||= recipient_award_element.element_children.at('RecipientUSAddress/AddressLine1Txt')&.text
+        parsed_recipient_name ||= recipient_award_element.element_children.at('RecipientNameBusiness/BusinessNameLine1')&.text
+        parsed_recipient_name ||= recipient_award_element.element_children.at('RecipientBusinessName/BusinessNameLine1Txt')&.text
+        parsed_recipient_name ||= recipient_award_element.element_children.at('RecipientBusinessName/BusinessNameLine1')&.text
 
-            parsed_recipient_city ||= recipient_award_element.element_children.at('AddressUS/City')&.text
-            parsed_recipient_city ||= recipient_award_element.element_children.at('USAddress/CityNm')&.text
-            parsed_recipient_city ||= recipient_award_element.element_children.at('USAddress/City')&.text
-            parsed_recipient_city ||= recipient_award_element.element_children.at('RecipientUSAddress/CityNm')&.text
+        parsed_recipient_address ||= recipient_award_element.element_children.at('AddressUS/AddressLine1')&.text
+        parsed_recipient_address ||= recipient_award_element.element_children.at('USAddress/AddressLine1Txt')&.text
+        parsed_recipient_address ||= recipient_award_element.element_children.at('USAddress/AddressLine1')&.text
+        parsed_recipient_address ||= recipient_award_element.element_children.at('RecipientUSAddress/AddressLine1Txt')&.text
 
-            parsed_recipient_state ||= recipient_award_element.element_children.at('AddressUS/State')&.text
-            parsed_recipient_state ||= recipient_award_element.element_children.at('USAddress/StateAbbreviationCd')&.text
-            parsed_recipient_state ||= recipient_award_element.element_children.at('USAddress/State')&.text
-            parsed_recipient_state ||= recipient_award_element.element_children.at('RecipientUSAddress/StateAbbreviationCd')&.text
+        parsed_recipient_city ||= recipient_award_element.element_children.at('AddressUS/City')&.text
+        parsed_recipient_city ||= recipient_award_element.element_children.at('USAddress/CityNm')&.text
+        parsed_recipient_city ||= recipient_award_element.element_children.at('USAddress/City')&.text
+        parsed_recipient_city ||= recipient_award_element.element_children.at('RecipientUSAddress/CityNm')&.text
 
-            parsed_recipient_zip ||= recipient_award_element.element_children.at('AddressUS/ZIPCode')&.text
-            parsed_recipient_zip ||= recipient_award_element.element_children.at('USAddress/ZIPCd')&.text
-            parsed_recipient_zip ||= recipient_award_element.element_children.at('USAddress/ZIPCode')&.text
-            parsed_recipient_zip ||= recipient_award_element.element_children.at('RecipientUSAddress/ZIPCd')&.text
+        parsed_recipient_state ||= recipient_award_element.element_children.at('AddressUS/State')&.text
+        parsed_recipient_state ||= recipient_award_element.element_children.at('USAddress/StateAbbreviationCd')&.text
+        parsed_recipient_state ||= recipient_award_element.element_children.at('USAddress/State')&.text
+        parsed_recipient_state ||= recipient_award_element.element_children.at('RecipientUSAddress/StateAbbreviationCd')&.text
 
-            receiver = Receiver.new(ein: parsed_recipient_ein, 
-                name: parsed_recipient_name, 
-                address: parsed_recipient_address, 
-                city: parsed_recipient_city, 
-                state: parsed_recipient_state, 
-                zip: parsed_recipient_zip)
-            
-            award.build_receiver(ein: parsed_recipient_ein, 
-                name: parsed_recipient_name, 
-                address: parsed_recipient_address, 
-                city: parsed_recipient_city, 
-                state: parsed_recipient_state, 
-                zip: parsed_recipient_zip)            
-            #TODO: slow, let's figure out saving in bulk
-            award.save
+        parsed_recipient_zip ||= recipient_award_element.element_children.at('AddressUS/ZIPCode')&.text
+        parsed_recipient_zip ||= recipient_award_element.element_children.at('USAddress/ZIPCd')&.text
+        parsed_recipient_zip ||= recipient_award_element.element_children.at('USAddress/ZIPCode')&.text
+        parsed_recipient_zip ||= recipient_award_element.element_children.at('RecipientUSAddress/ZIPCd')&.text
+
+        receiver = Receiver.new(ein: parsed_recipient_ein, 
+            name: parsed_recipient_name, 
+            address: parsed_recipient_address, 
+            city: parsed_recipient_city, 
+            state: parsed_recipient_state, 
+            zip: parsed_recipient_zip)
+
+        return receiver
     end
 end
